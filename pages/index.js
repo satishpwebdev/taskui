2736dccfa9bb8c9ba32bfc252daf87c56642be41
Satish <satish.p@ensologic.com>
2023-05-21 05:04:37 +0530

    profile button added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import TaskList from "@/components/tasks";
+import Addtask from "@/components/addtask";
 
 export default function Home() {
    return (
       <>
-               <TaskList></TaskList>
+         <TaskList></TaskList>
       </>
    );
 }
