ab9957eb8ff4d89fbfc798ad470fdc728a7933b3
satishpwebdev <72158548+satishpwebdev@users.noreply.github.com>
2023-09-30 16:31:18 +0530

    Update url.js

--- constants/url.js
+++ constants/url.js
@@ -1,2 +1,3 @@
-export const liveURL = 'https://taskbe.onrender.com'
+// export const liveURL = 'https://taskbe.onrender.com'
+export const liveURL = 'https://taskbe.cyclic.cloud'
 export const testURL = 'http://localhost:8000'
